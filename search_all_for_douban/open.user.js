86c047c68c8841e9e66fd93fe6092c9d78ecddf8
skypesky <2565978507@qq.com>
2018-08-16 14:20:10 +0800

    update douban and create lib

--- search_all_for_douban/open.user.js
+++ search_all_for_douban/open.user.js
@@ -12,9 +12,10 @@
 // @require      https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
 
+
 // 全局变量
 const config = {
-    keyword: `keyword`,
+    keyword: `#keyword#`,
     showLength: 5
 };
 
@@ -23,150 +24,256 @@ const config = {
 
     'use strict';
 
-
-    const subtitleMap = [
-        {
-            url: `http://www.rs05.com/search.php?s=${config.keyword}`,
+    // 影片资源
+    const videoResourceList = [{
             name: `RS05`,
-            success: {
-                selector: `#movielist .pure-g`,
-            }
+            description: `RS05(视频)`,
+            url: `http://www.rs05.com/search.php?s=${config.keyword}`,
+            successSelector: `#movielist .pure-g`,
+            type: `video`,
+            typeName: `视频下载`
         },
         {
-            url: `http://www.iloldytt.cc/search.html?title=${config.keyword}`,
+            name: `看天堂`,
+            description: `看天堂(视频)`,
+            url: `http://www.kantiantang.com/search?text=${config.keyword}`,
+            successSelector: `.col-md-8 .page-header`,
+            type: `video`,
+            typeName: `视频下载`
+        },
+        {
             name: `电影天堂`,
-            success: {
-                selector: `browse-inner .img-responsive`,
-            }
+            description: `电影天堂(视频)`,
+            url: `http://www.iloldytt.cc/search.html?title=${config.keyword}`,
+            successSelector: `browse-inner .img-responsive`,
+            type: `video`,
+            typeName: `视频下载`
         },
         {
-            url: `https://www.qpg123.com/search/?wd=${config.keyword}`,
             name: `青苹果影院`,
-            success: {
-                selector: `img.lazy`,
-            }
+            description: `青苹果影院(视频)`,
+            url: `https://www.qpg123.com/search/?wd=${config.keyword}`,
+            successSelector: `img.lazy`,
+            type: `video`,
+            typeName: `视频下载`
         },
         {
-            url: `http://www.xunyingwang.com/search?q=${config.keyword}`,
             name: `迅影网`,
-            success: {
-                selector: `.img-thumbnail`,
-            }
+            description: `迅影网(视频)`,
+            url: `http://www.xunyingwang.com/search?q=${config.keyword}`,
+            successSelector: `.img-thumbnail`,
+            type: `video`,
+            typeName: `视频下载`
         },
         {
-            url: `http://www.bd-film.co/search.jspx?q=${config.keyword}`,
             name: `BD影视`,
-            success: {
-                selector: `.list-item`,
-            }
+            description: `BD影视(视频)`,
+            url: `http://www.bd-film.co/search.jspx?q=${config.keyword}`,
+            successSelector: `.list-item`,
+            type: `video`,
+            typeName: `视频下载`
         }
     ];
+
+    // 字幕资源
+    const subtitleResourceList = [{
+        name: `字幕库`,
+        url: `https://www.zimuku.cn/search?q=${config.keyword}`,
+        description: `字幕库(字幕)`,
+        successSelector: `.tborder2`,
+        type: `subtitle`,
+        typeName: `字幕`
+    }];
+
+    // 运行在哪个网站?
+    const websiteConfigList = [{
+        name: `豆瓣电影频道网站`,
+        description: `豆瓣网 | 查找电影,电视剧`,
+        url: `http*://movie.douban.com/subject/*`,
+        rule: /\/\/movie.douban.com\/subject\/*/,
+        resourceList: [
+            videoResourceList
+        ],
+        keywordSelector: `#content > h1 > span:nth-child(1)`,
+        showView: {
+            selector: `.subjectwrap`,
+            position: `after`
+        }
+    }];
+
     $(function () {
-        Server.run(subtitleMap);
-    })
+        Controller.run(websiteConfigList);
+    });
+
 })();
 
-const Server = {
-    videoSearchList: [],
-    title: null,
-    //计数器
-    count: 0,
-    run: function (subtitleMap) {
-        // 初始化计数器
-        this.count = 0;
-        // get title
-        this.title = this.getTitle();
 
-        for (let i = 0; i < subtitleMap.length; ++i) {
-            this.getData(subtitleMap[i]);
+// 对于resource来说,拿到应有的属性,就可以获得结果,通过回调函数或者返回结果进行下一步处理
+function Resource() {
+    // 未替换的搜索链接
+    this._originUrl = ``,
+        // 搜索资源的关键字
+        this._keyword = ``,
+        // 资源检索成功的元素的选择器
+        this._successSelector = ``,
+        // 真实的地址
+        this._realUrl = ``
+}
+
+/**
+ ** @desc: 初始化参数
+ ** @param: originUrl => 未替换的搜索链接
+ **         keyword => 查询的关键字
+ **         successSelector => 资源检索成功的元素的选择器
+ ** 
+ */
+Resource.prototype.init = function (originUrl, keyword, successSelector) {
+    this._originUrl = originUrl;
+    this._keyword = keyword;
+    this._successSelector = successSelector;
+    // 获取真实要访问的地址
+    this.getRealUrl();
+};
+
+/**
+ ** @desc: 检测资源是否存在,存在返回true,不存在返回false
+ ** @param: selector => 选择器
+ **         responseText => 响应的html文本信息
+ ** @return: 资源存在返回true,否则否则返回false
+ */
+Resource.prototype.exist = function (responseText) {
+    return $(this._successSelector, responseText).length > 0;
+};
+
+/**
+ ** @desc: 通过替换字符串,获取真实的查询网址
+ ** @param:
+ ** @return: 返回真实的查询网址
+ */
+Resource.prototype.getRealUrl = function () {
+    this._realUrl = this._originUrl.replace(config.keyword, this._keyword);
+    return this._realUrl;
+}
+
+/**
+ ** @desc: 检索资源,检索成功返回真实链接,检索失败返回null 
+ ** @param: callback => 回调函数的地址引用
+ ** @return: 
+ */
+Resource.prototype.search = function (callback) {
+    GM_xmlhttpRequest({
+        method: `GET`,
+        url: this._realUrl,
+        data: {
+            title: this._keyword
+        },
+        onload: (result) => {
+            // 回调函数
+            callback(result, this.exist(result.responseText));
+        }
+    });
+}
+
+
+const ServerController = {
+    _websiteConfig: null,
+    _keyword: ``,
+    // 初始化配置
+    init: function (websiteConfig) {
+        this._websiteConfig = websiteConfig;
+        // 要查询的关键字
+        this.getKeyword();
+    },
+    // 运行
+    run: function () {
+
+        // 查询成功后的资源列表
+        let successResourceList = [];
+
+        // 已查询的数目
+        let searchNumber = 0;
+        // 总共需要查询的数目
+        let totalNumber = 0;
+
+        // 查询
+        for (let i = 0; i < this._websiteConfig.resourceList.length; ++i) {
+            // 统计总数
+            totalNumber += this._websiteConfig.resourceList[i].length;
+            for (let j = 0; j < this._websiteConfig.resourceList[i].length; ++j) {
+                let resource = new Resource();
+                // 初始化资源属性
+                resource.init(this._websiteConfig.resourceList[i][j].url, this._keyword, this._websiteConfig.resourceList[i][j].successSelector);
+                // 查询
+                resource.search((result, flag) => {
+                    searchNumber++;
+                    if (flag) { //查询成功
+                        console.log("search success!");
+                        successResourceList.push({
+                            description: this._websiteConfig.resourceList[i][j].description,
+                            link: resource.getRealUrl(),
+                            type: this._websiteConfig.resourceList[i][j].type,
+                            typeName: this._websiteConfig.resourceList[i][j].typeName
+                        });
+                    }
+                });
+            }
         }
 
+        // 检测查询是否全部完成
         let task = setInterval(() => {
-            if (this.count == subtitleMap.length) {
-                if (this.videoSearchList.length > 0) {
-                    this.addStyle(this.videoSearchList);
-                    clearInterval(task);
-                } else {
-                    this.addEmptyStyle("抱歉,这部电影未找到适配的下载源!");
-                }   
+            // 全部查询完成,显示查询结果,关闭定时器
+            if (searchNumber == totalNumber) {
+                console.log(`search success!`);
+                this.showView(successResourceList);
+                clearInterval(task);
             }
-        }, 50);
-
+        }, 100);
     },
-    // 查找成功,返回true,否则返回false
-    searchSuccess: (success, responseText) => {
-        return $(success.selector, responseText).length > 0;
+    // 获取要查询的关键字
+    getKeyword: function () {
+        this._keyword = $(this._websiteConfig.keywordSelector).text().split(` `)[0];
+        return this._keyword;
     },
-    // 获取电影名称
-    getTitle: function () {
-        return $(`#content > h1 > span:nth-child(1)`).text().split(` `)[0];
-    },
-    // 添加样式
-    addStyle: function (data) {
-        let table = `<br /><hr style="border: 1px solid #e5e5e5" />`;
-        table += `<table style='text-align: center; padding: 14px; width: 100%;'>`;
+    // 显示资源在视图上
+    showView: function (data) {
+        let table = ``;
+        table += `<table style='text-align: center; padding: 14px; width: 100%;border: 2px solid #ddd; margin: 20px auto;'>`;
         for (let i = 0; i < data.length; i = i + config.showLength) {
 
             table += `<tr>`;
 
             for (let j = i; j < i + config.showLength && data[j]; ++j) {
-                table += `<td><a href='${data[j].link}' target='_blank'>${data[j].name}</td>`;
+                table += `<td style="padding-top: 10px; padding-bottom: 10px;"><a href='${data[j].link}' target='_blank'>${data[j].description}</td>`;
             }
 
             table += `</tr>`;
         }
         table += `</table>`;
-        table += `<hr style="border: 1px solid #e5e5e5" /><br />`;
-        $(`.subjectwrap`).after(table);
-    },
-    addEmptyStyle: function (message) {
-        let table = `<br /><hr style="border: 1px solid #e5e5e5" />`;
-        table += `<p>${message}</p>`;
-        table += `<hr style="border: 1px solid #e5e5e5" /><br />`;
-        $(`.subjectwrap`).after(table);
-    },
-    getData: function (value) {
-        let _this = this;
-        // 获取跳转链接
-        let link = value.url.replace(config.keyword, this.title);
-        console.log(`link is ${link}`);
-        GM_xmlhttpRequest({
-            method: `GET`,
-            url: link,
-            data: {
-                title: this.title
-            },
-            onload: (res) => {
-                this.callback(value, link, res);
-            }
-        });
-    },
-    callback: function (value, link, res) {
-        // 查询成功
-        if (this.searchSuccess(value.success, res.responseText)) {
-            // 添加有效的资源到队列尾部
-            this.videoSearchList[this.videoSearchList.length] = {
-                link: link,
-                name: value.name
-            };
-        }
-        // 计数器+1
-        ++this.count;
+        $(this._websiteConfig.showView.selector).after(table);
     }
 }
 
-// 验证工具类
-const Validate = {
-    /*
-    ** @desc: 验证对象
-    ** @param: object
-    ** @return: object不为null且不为undefined时返回true,否则返回false
-    */
-    checkObject: function (object) {
-        console.log(!object);
-        if (!object || object == undefined) {
-            return false;
+// 选择执行的控制器
+const Controller = {
+    // 匹配网站
+    select: function (configList, keyword) {
+        for (let i = 0; i < configList.length; ++i) {
+            if (configList[i].rule.test(keyword)) {
+                return i;
+            }
         }
-        return true;
+        return -1;
+    },
+    // 匹配执行的控制器
+    run: function (websiteConfigList) {
+        let keyword = window.location.href;
+        let index = this.select(websiteConfigList, keyword);
+        if (index != -1) { //匹配成功
+            console.log("匹配成功! " + websiteConfigList[index].description);
+            ServerController.init(websiteConfigList[index]);
+            ServerController.run();
+        } else { //匹配失败
+            console.log("匹配失败!");
+        }
     }
 }
